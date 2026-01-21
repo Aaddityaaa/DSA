38e2aa452dae203b4422e9be49b410267cb5d780
Aaddityaaa <adityau5090@gmail.com>
2026-01-21 07:34:34 +0530

    implementing deletion logic in bst

--- tree/03_binary_search_tree.cpp
+++ tree/03_binary_search_tree.cpp
@@ -60,6 +60,27 @@ public:
         }
     }
 
+    // deletion in binary search tree
+    Node* del(Node* root, int val){
+        if(root == nullptr){
+            return root;
+        }
+        if(val <= root->data){
+            root->left = del(root->left, val);
+        }
+        else if(val > root->data){
+            root->right = del(root->right, val);
+        }
+        else{
+            if(root->left == nullptr){
+                return root->right;
+            }
+            if(root->right == nullptr){
+                return root->left;
+            }
+        }
+    }
+
     void inOrder(Node *root)
     {
         if (root == nullptr)
